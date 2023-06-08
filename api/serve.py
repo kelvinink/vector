38e7e791842aa556df4309d36ddd3ae8671aadba
ZHIHAO LIANG <zhihao@ZHIHAOs-Mac-mini.local>
2023-06-08 22:37:45 +0800

    hello world

--- api/serve.py
+++ api/serve.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
+# Create the Flask application instance
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+# Define a route and corresponding function
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+# Run the application if this script is executed directly
+if __name__ == '__main__':
+    app.run()
